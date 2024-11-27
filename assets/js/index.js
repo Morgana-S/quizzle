9c865c87c126ed56b3e985df97587715b5c430a7
Morgana-S <st35giv9@students.codeinstitute.net>
2024-11-27 15:45:45 +0000

    Create list of questions for quiz

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,77 @@
 function displayMenu() {
     document.getElementById('dropdown-links').classList.toggle('show-dropdown');
 }
+
+// List of Questions and Answers
+let questions = [
+    {
+        text: 'What is the capital city of Argentina?',
+        answer1: ['Buenos Aires', true],
+        answer2: ['Santiago', false],
+        answer3: ['Bogotá', false],
+        answer4: ['Sucre', false]
+    },
+    {
+        text: 'How many bones are in an adult human body?',
+        answer1: ['300', false],
+        answer2: ['99', false],
+        answer3: ['206', true],
+        answer4: ['50', false]
+    },
+    {
+        text: 'What is the chemical symbol for Silver?',
+        answer1: ['Pt', false],
+        answer2: ['Au', false],
+        answer3: ['Ag', true],
+        answer4: ['Sn', false]
+    },
+    {
+        text: 'How many oceans are there on planet Earth?',
+        answer1: ['4', false],
+        answer2: ['7', true],
+        answer3: ['8', false],
+        answer4: ['5', false],
+    },
+    {
+        text: 'Which Renaissance Artist painted the ceiling of the Sistine Chapel?',
+        answer1: ['Leonardo Da Vinci', false],
+        answer2: ['Michelangelo', true],
+        answer3: ['Raphael', false],
+        answer4: ['Donatello', false]
+    },
+    {
+        text: 'Which planet in the solar system is the hottest?',
+        answer1: ['Mars', false],
+        answer2: ['Saturn', false],
+        answer3: ['Venus', true],
+        answer4: ['Mercury', false]
+    },
+    {
+        text: '"Farfalle" is a type of:',
+        answer1: ['Rice', false],
+        answer2: ['Bean', false],
+        answer3: ['Pasta', true],
+        answer4: ['Vegetable', false]
+    },
+    {
+        text: 'Nike is the greek goddess of:',
+        answer1: ['Victory', true],
+        answer2: ['Love', false],
+        answer3: ['War', false],
+        answer4: ['Wine', false],
+    },
+    {
+        text: 'England won the FIFA World Cup in which year?',
+        answer1: ['1934', false],
+        answer2: ['1970', false],
+        answer3: ['1966', true],
+        answer4: ['2006', false]
+    },
+    {
+        text: 'Which river flows through the Grand Canyon?',
+        answer1: ['Mississipi River', false],
+        answer2: ['Ohio River', false],
+        answer3: ['Missouri River', false],
+        answer4: ['Colorado River', true]
+    }
+]
